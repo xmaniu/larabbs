b9e2799f6468e91f3cb07bd17429d61709bfa4f2
33 <858566651@qq.com>
2019-09-05 15:21:17 +0800

    静态文件浏览器缓存问题

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            由 <a href="https://weibo.com/u/2032529904" target="_blank">Mr叶枫</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="float-right"><a href="mailto:858566651@qq.com">联系我们</a></p>
