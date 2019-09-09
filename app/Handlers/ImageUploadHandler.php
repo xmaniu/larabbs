bcc4e9197b537e9caf707e49ba571c399de181f2
33 <858566651@qq.com>
2019-09-09 17:18:21 +0800

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,13 +1,14 @@
 <?php
 namespace App\Handlers;
 use Illuminate\Support\Str;
+use Image;
 
 class ImageUploadHandler
 {
     // 只允许以下后缀名的图片文件上传
     protected $allowed_ext = ['png','jpg','gif','jpeg'];
 
-    public function save($file,$folder,$file_prefix)
+    public function save($file,$folder,$file_prefix, $max_width = false)
     {
         // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
         // 文件夹切割能让查找效率更高。
@@ -30,8 +31,27 @@ class ImageUploadHandler
         // 将图片移动到我们的目标存储路径中
         $file->move($upload_path,$filename);
 
+        if($max_width && $extension != 'gif'){
+            $this->reduceSize($upload_path.'/'.$filename,$max_width);
+        }
+
         return[
             'path'=>config('app.url')."/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path,$max_width)
+    {
+        $image = Image::make($file_path);
+
+        $image->resize($max_width,null,function ($constraint){
+            // 设定宽度是 $max_width，高度等比例缩放
+            $constraint->aspectRatio();
+
+            // 防止裁图时图片尺寸变大
+            $constraint->upsize();
+        });
+
+        $image->save();
     }
 }
