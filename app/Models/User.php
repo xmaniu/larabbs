106f441dd28c576faf9f8bac0e5897d1a00f1531
33 <858566651@qq.com>
2019-09-30 15:17:43 +0800

    用户最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Traits\ActiveUserHelper;
+use App\Models\Traits\LastActivedAtHelper;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -15,6 +16,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
     use MustVerifyEmailTrait;
     use HasRoles;
     use ActiveUserHelper;
+    use LastActivedAtHelper;
     use Notifiable {
         notify as protected laravelNotify;
     }
