8c78f10c90e152852b72c972e3e0107b5d011c2f
33 <858566651@qq.com>
2019-09-30 10:42:37 +0800

    站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://weibo.com/u/2032529904" target="_blank">Mr叶枫</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="float-right"><a href="mailto:858566651@qq.com">联系我们</a></p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
